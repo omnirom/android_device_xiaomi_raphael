6e640112dc563672877ea9baf1a88e220c3bb0e2
Bruno Martins <bgcngm@gmail.com>
2020-06-19 16:58:08 +0530

    raphael: re-import xiaomi parts from los tree
    Change-Id: I2f4807e15a29e426c2566c3f26a518d6ea8d97f5
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Abhishek Aggarwal <warabhishek@gmail.com>
    Signed-off-by: Millan Mishra <3ztremors@gmail.com>

--- XiaomiParts/Android.mk
+++ XiaomiParts/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -27,9 +27,12 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     vendor.xiaomi.hardware.displayfeature-V1.0-java \
     vendor.xiaomi.hardware.motor-V1.0-java
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/xres
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(TOP)/packages/resources/devicesettings/res
 
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
