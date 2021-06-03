ee087a1e47d05a71ba2aa47dbb9eeb0286850cf8
Menno van Grinsven <scanno71@gmail.com>
2021-06-03 17:12:11 +0200

    raphael: update redfin fingerprint
    Change-Id: Ice0de8e5032454afa6a8b4a7e1d86718daa0dcd4

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -49,21 +49,21 @@ void load_raphaelglobal() {
     property_override("ro.product.model", "Mi 9T Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "redfin-user 11 RQ1A.210205.004 7038034 release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210405.005 7181113 release-keys");
 }
 
 void load_raphaelin() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "redfin-user 11 RQ1A.210205.004 7038034 release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210405.005 7181113 release-keys");
 }
 
 void load_raphael() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "redfin-user 11 RQ1A.210205.004 7038034 release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210405.005 7181113 release-keys");
 }
 
 
@@ -93,7 +93,7 @@ void vendor_load_properties() {
     std::string region = android::base::GetProperty("ro.boot.hwc", "");
 
     property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
-        "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys");
+        "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys");
 
     if (region.find("CN") != std::string::npos) {
         load_raphael();
